974059fab6fb200b92931962cdc3f58a31d6e3cc
Kagaya2004 <kaykyogaya@gmail.com>
2024-11-05 21:50:32 -0300

    Revert "Retornar"
    This reverts commit 8691ccb1ab612df73554a05c4fd81cd4bc86f927.

---
+++ IFSPStore.Services/Validators/CidadeValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using IFSPStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFSPStore.Service.Validators
+{
+    public class CidadeValidator : AbstractValidator<Cidade>
+    {
+        public CidadeValidator()
+        {
+            RuleFor(c => c.Nome)
+                .NotEmpty().WithMessage("Por favor informe o nome.")
+                .NotNull().WithMessage("Por favor informe o nome.")
+                .Length(50)
+                .WithMessage("Nome pode ter no máximo 50 caracteres.");
+
+            RuleFor(c => c.Estado)
+                .NotEmpty().WithMessage("Por favor informe o estado.")
+                .NotNull().WithMessage("Por favor informe o estado.");
+        }
+    }
+}
