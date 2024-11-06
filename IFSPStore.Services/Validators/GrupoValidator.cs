1583cefd8543135695eb54d343e613befd417275
Kagaya2004 <kaykyogaya@gmail.com>
2024-11-05 21:40:30 -0300

    IFSPStore com Services

---
+++ IFSPStore.Services/Validators/GrupoValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using IFSPStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFSPStore.Service.Validators
+{
+    public class GrupoValidator : AbstractValidator<Grupo>
+    {
+        public GrupoValidator()
+        {
+            RuleFor(c => c.Nome)
+                .NotEmpty().WithMessage("Por favor informe o nome do grupo.")
+                .NotNull().WithMessage("Por favor informe o nome do grupo.");
+        }
+    }
+}
---
+++ IFSPStore.Services/Validators/UsuarioValidator.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using IFSPStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFSPStore.Service.Validators
+{
+    public class UsuarioValidator : AbstractValidator<Usuario>
+    {
+        public UsuarioValidator()
+        {
+            RuleFor(c => c.Nome)
+                .NotEmpty().WithMessage("Nome do usuário obrigatório.")
+                .NotNull().WithMessage("Nome do usuário obrigatório.");
+            RuleFor(c => c.Senha)
+                .NotEmpty().WithMessage("Senha obrigatório.")
+                .NotNull().WithMessage("Senha obrigatório.")
+                .MinimumLength(8).WithMessage("A senha precisa possuir,no mínimo, 8 caracteres")
+                .Matches(@"[A-Z]+").WithMessage("A senha precisa possuir, pelo menos, um caractere maíusculo.")
+                .Matches(@"[a-z]+").WithMessage("A senha precisa possuir, pelo menos, um caractere minúsculo")
+                .Matches(@"[0-9]+").WithMessage("A senha precisa possuir, pelo menos, um caractere numérico")
+                .Matches(@"[\!\?\*\.]+").WithMessage("a senha precisa possuir, pelo menos, um caractere desses caracteres: ! ? * .");
+            RuleFor(c => c.Email)
+                .NotEmpty().WithMessage("E-mail obrigatório.")
+                .NotNull().WithMessage("E-mail obrigatório.")
+                .EmailAddress().WithMessage("E-mail inválido.");
+            RuleFor(c => c.Login)
+                .NotEmpty().WithMessage("Login obrigatório.")
+                .NotNull().WithMessage("Login obrigatório.");
+
+        }
+    }
+}
