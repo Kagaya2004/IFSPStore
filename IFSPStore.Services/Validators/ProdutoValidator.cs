61b2a84a1f1193ebc529c22b2eee7cbaa14e3d44
Kagaya2004 <kaykyogaya@gmail.com>
2024-11-12 08:38:27 -0300

    Sou noob com o Github

---
+++ IFSPStore.Services/Validators/ProdutoValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using IFSPStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFSPStore.Service.Validators
+{
+    public class ProdutoValidator : AbstractValidator<Produto> 
+    {
+        public ProdutoValidator()
+        {
+            RuleFor(c => c.Nome)
+                .NotEmpty().WithMessage("Por favor informe o produto.")
+                .NotNull().WithMessage("Por favor informe o produto.");
+            RuleFor(c => c.Preco)
+                .NotEmpty().WithMessage("Por favor informe o preço.")
+                .NotNull().WithMessage("Por favor informe o preço.");
+            RuleFor(c => c.Quantidade)
+                .NotEmpty().WithMessage("Por favor informe a quantidade.")
+                .NotNull().WithMessage("Por favor informe a quantidade.")
+                .LessThan(0).WithMessage("A quantidade deve ser acima de zero (0).");
+            RuleFor(c => c.UnidadeVenda)
+                .NotEmpty().WithMessage("Por favor informe a unidade de medida do produto.")
+                .NotNull().WithMessage("Por favor informe a unidade de medida do produto.");
+            RuleFor(c => c.Grupo)
+                .NotEmpty().WithMessage("Por favor informe o grupo do produto.")
+                .NotNull().WithMessage("Por favor informe o grupo do produto.");
+        }
+    }
+}
---
+++ IFSPStore.Services/Validators/VendaValidator.cs
@@ -0,0 +1,58 @@
+﻿using FluentValidation;
+using IFSPStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFSPStore.Service.Validators
+{
+    public class VendaValidator : AbstractValidator<Venda>
+    {
+        public VendaValidator()
+        {
+            RuleFor(c => c.Data)
+                .NotEmpty().WithMessage("Por favor informe a data da venda.")
+                .NotNull().WithMessage("Por favor informe a data da venda.");
+            RuleFor(c => c.ValorTotal)
+                .NotEmpty().WithMessage("Por favor informe o valor total da venda.")
+                .NotNull().WithMessage("Por favor informe o valor total da venda.")
+                .LessThan(0).WithMessage("O valor total deve ser acima de zero (0).");
+            RuleFor(c => c.Usuario)
+                .NotEmpty().WithMessage("Por favor informe a quantidade.")
+                .NotNull().WithMessage("Por favor informe a quantidade.");
+            RuleFor(c => c.Cliente)
+                .NotEmpty().WithMessage("Por favor informe a unidade de medida do produto.")
+                .NotNull().WithMessage("Por favor informe a unidade de medida do produto.");
+            RuleFor(c => c.Itens)
+                .NotEmpty().WithMessage("Por favor informe o grupo do produto.")
+                .NotNull().WithMessage("Por favor informe o grupo do produto.");
+        }
+    }
+
+    public class VendaItemValidator : AbstractValidator<VendaItem>
+    {
+        public VendaItemValidator()
+        {
+            RuleFor(c => c.Produto)
+                    .NotEmpty().WithMessage("Por favor informe o produto.")
+                    .NotNull().WithMessage("Por favor informe o produto.");
+            RuleFor(c => c.Quantidade)
+                .NotEmpty().WithMessage("Por favor informe a quantidade.")
+                .NotNull().WithMessage("Por favor informe a quantidade.")
+                .LessThan(0).WithMessage("A quantidade deve ser acima de zero (0).");
+            RuleFor(c => c.ValorUnitario)
+                .NotEmpty().WithMessage("Por favor informe o valor unitário do item.")
+                .NotNull().WithMessage("Por favor informe o valor unitário do item.")
+                .LessThan(0).WithMessage("O valor unitário deve ser acima de zero (0).");
+            RuleFor(c => c.ValorTotal)
+                .NotEmpty().WithMessage("Por favor informe o valor total.")
+                .NotNull().WithMessage("Por favor informe o valor total.")
+                .LessThan(0).WithMessage("O valor total deve ser acima de zero (0).");
+            RuleFor(c => c.Venda)
+                .NotEmpty().WithMessage("Por favor informe o grupo do produto.")
+                .NotNull().WithMessage("Por favor informe o grupo do produto.");
+        }
+    }
+}
